1a66dd3da3b5ef84ae3a344d5323dcd578a64ff0
Oliver Roick <oliver@roick.email>
2024-12-11 15:17:32 +1100

    Add rendering for array-type assistant messages

--- src/components/MessageAssistant.jsx
+++ src/components/MessageAssistant.jsx
@@ -1,17 +1,53 @@
 import T from "prop-types";
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import Markdown from "react-markdown";
 
 function MessageAssistant({ message }) {
-  return (
-    <Box mb="4" p="2" bgColor="gray.50" borderRadius="4px">
-      <Markdown>{message}</Markdown>
-    </Box>
-  );
+  if (typeof message === "string" || message instanceof String) {
+    return (
+      <Box mb="4" p="2" bgColor="gray.50" borderRadius="4px">
+        <Markdown>{message}</Markdown>
+      </Box>
+    );
+  } else {
+    return (
+      <Box mb="4" p="2" bgColor="gray.50" borderRadius="4px">
+        {message.map((messagePart) => {
+          const { index, type } = messagePart;
+          if (type === "text") {
+            return <Markdown key={index}>{messagePart.text}</Markdown>;
+          } else {
+            return <Button size="xs" mt="4" key={index}>{messagePart.name}</Button>;
+          }
+        })}
+      </Box>
+    );
+  }
 }
 
+const TextMessageType = T.shape({
+  index: T.number.isRequired,
+  type: "text",
+  text: T.string.isRequired
+});
+
+const ToolUseMessageType = T.shape({
+  id: T.string.isRequired,
+  index: T.number.isRequired,
+  input: T.object,
+  name: T.string.isRequired,
+  partial_json: T.string.isRequired,
+  type: "tool_use"
+});
+
 MessageAssistant.propTypes = {
-  message: T.string.isRequired
+  message: T.oneOfType([
+    T.string.isRequired,
+    T.arrayOf(T.oneOfType([
+      TextMessageType,
+      ToolUseMessageType
+    ])).isRequired
+  ])
 };
 
 export default MessageAssistant;
