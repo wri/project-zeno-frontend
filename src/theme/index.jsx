85ef6e28aa8838479ed8e649c8b9b6ae9bd62a8f
Marc Farra <marcfarra@gmail.com>
2024-12-16 21:24:23 +0000

    set font in app to IBM plex sans

--- src/theme/index.jsx
+++ src/theme/index.jsx
@@ -1,5 +1,16 @@
+"use client";
+
 import { createSystem, defaultConfig } from "@chakra-ui/react";
 
-const config = {};
+const config = {
+  theme: {
+    tokens: {
+      fonts: {
+        body: { value: "IBM Plex Sans" },
+        heading: { value: "IBM Plex Sans" },
+      }
+    }
+  }
+};
 
 export default createSystem(defaultConfig, config);
