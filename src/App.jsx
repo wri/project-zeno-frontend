2ada424ff4a683e18cf704687e6cb0bb91172ced
Oliver Roick <oliver@roick.email>
2024-12-16 17:26:48 +1100

    Update page layout, add page header

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import { Box, Grid } from "@chakra-ui/react";
+import { Box, Grid, Text } from "@chakra-ui/react";
 import Providers from "./Providers";
 import { ChatInput, ChatOutput, Map } from "./components";
 
 function App() {
   return (
     <Providers>
-      <Box position="absolute" top="0" left="0" bottom="0" right="0">
-        <Map />
-      </Box>
-      <Grid
-        position="absolute"
-        top="4"
-        left="4"
-        bottom="4"
-        width="80"
-        borderRadius="4px"
-        py="4"
-        bgColor="rgba(255,255,255,.5)"
-        gap="4"
-        templateRows="1fr max-content"
-        backdropFilter="blur(8px)"
-      >
-        <Box overflowY="auto" px="4">
-          <ChatOutput />
+      <Grid position="absolute" top="0" left="0" bottom="0" right="0" templateRows="max-content 1fr">
+        <Box bgColor="blue.50" borderBottom="1px solid" borderBottomColor="blackAlpha.200" shadow="sm" px="8" py="4">
+          <Text as="h1" color="blue.900" fontWeight="700">Land Carbon Lab - Zeno</Text>
         </Box>
-        <Box px="4">
-          <ChatInput />
-        </Box>
+        <Grid templateColumns="300px 1fr" p="8" gap="2">
+          <Grid
+            gap="4"
+            templateRows="1fr max-content"
+            border="1px solid"
+            borderColor="blackAlpha.200"
+            borderRadius="lg"
+            shadow="sm"
+            p="4"
+          >
+            <Box overflowY="auto">
+              <ChatOutput />
+            </Box>
+            <Box>
+              <ChatInput />
+            </Box>
+          </Grid>
+          <Box
+            border="1px solid"
+            borderColor="blackAlpha.200"
+            borderRadius="lg"
+            shadow="sm"
+            overflow="hidden"
+          >
+            <Map />
+          </Box>
+        </Grid>
       </Grid>
     </Providers>
   );
