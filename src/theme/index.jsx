b54a9de882d034d51fb463fc2ade5334ff6b2f14
LanesGood <Lane@developmentseed.org>
2024-12-19 11:33:43 -0500

    Add colors to theme, update app bg

--- src/theme/index.jsx
+++ src/theme/index.jsx
@@ -8,9 +8,38 @@ const config = defineConfig({
       fonts: {
         body: { value: "IBM Plex Sans" },
         heading: { value: "IBM Plex Sans" },
-      }
-    }
-  }
+      },
+      colors: {
+        blue: {
+          50: "#ebf9ff",
+          100: "#d1f1ff",
+          200: "#aee7ff",
+          300: "#76dbff",
+          400: "#35c4ff",
+          500: "#07a0ff",
+          600: "#007bff",
+          700: "#0062ff",
+          800: "#0051d7",
+          900: "#0049a8", //primary
+          950: "#062d65",
+        },
+
+        lime: {
+          50: "#fcfee7",
+          100: "#f7fbcc",
+          200: "#eef89e",
+          300: "#def066",
+          400: "#cbe437",
+          500: "#adca18", //primary
+          600: "#87a10f",
+          700: "#667b10",
+          800: "#516113",
+          900: "#445215",
+          950: "#242e05",
+        },
+      },
+    },
+  },
 });
 
 export default createSystem(defaultConfig, config);
