c721af712c6e88ecd831f3e996bca19628d64461
Marc Farra <marcfarra@gmail.com>
2025-01-24 13:15:19 +0200

    fix merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { Box, Flex, Grid, Image, Text } from "@chakra-ui/react";
-import { useAtomValue } from "jotai";
+import { Box, Grid } from "@chakra-ui/react";
 import Providers from "./Providers";
 import { ChatInput, ChatOutput, Map } from "./components";
 import BarChart from "./components/BarChart";
 import FilmStrip from "./components/FilmStrip";
 import TabbedPanel from "./components/TabbedPanel";
-import logo from "/logo.svg";
 import ExportPane from "./components/ExportPane";
 import GlobalHeader from "./components/globalheader";
 
@@ -35,26 +33,8 @@ function App() {
         templateRows="min-content minmax(0, 1fr)"
         bg="gray.50"
       >
-        <Box bgColor="white" shadow="sm" px="8" py="4">
-          <Flex gap={12} alignItems="center">
-            <Image src={logo} height="40px" alt="WRI Land Carbon Lab logo" />
-            <Text 
-              fontFamily="mono"
-              fontVariantNumeric="slashed-zero"
-              fontFeatureSettings="'ss03'"
-              letterSpacing="0.75px"
-              fontSize="sm"
-              lineHeight="100%"
-              textTransform="uppercase"
-              fontWeight="500"
-              title="Project Zeno"
-            >
-              Pr0ject <br /> Zen0_
-            </Text>
-          </Flex>
-        </Box>
-        <Grid templateColumns="28rem 1fr" p="6" gap="2">
         <GlobalHeader />
+        <Grid templateColumns="28rem 1fr" p="6" gap="2">
           <Grid
             gap="4"
             templateRows="1fr max-content"
