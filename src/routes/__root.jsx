c25eb2f395d800fdb3c6d6fc84886368b4153dcb
LanesGood <Lane@developmentseed.org>
2025-02-25 14:13:27 -0500

    Add mode menu control to header, remove route nav bar

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,18 +1,9 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/alerting" className="[&.active]:font-bold">
-          Alerting
-        </Link>{" "}
-        <Link to="/monitoring" className="[&.active]:font-bold">
-          Monitoring
-        </Link>
-      </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
