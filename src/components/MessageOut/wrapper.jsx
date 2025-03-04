a9919bb45962a6acd04b54c50040cd530274bbac
LanesGood <Lane@developmentseed.org>
2025-03-04 15:56:26 -0500

    [OT] Fix logo DOM warnings

--- src/components/MessageOut/wrapper.jsx
+++ src/components/MessageOut/wrapper.jsx
@@ -6,7 +6,7 @@ function MessageOutWrapper({ children }) {
   return (
     <Box mb="4" p="2" bgColor="bg.muted" borderRadius="md" borderWidth={1} borderColor="border" flexGrow="1" overflow="hidden">
       <Box float="left" mr="1" mt="0.5">
-        <LclLogo width="12" avatarOnly fill="var(--chakra-colors-bg-inverted)" float="left" />
+        <LclLogo width={12} avatarOnly fill="var(--chakra-colors-bg-inverted)" float="left" />
       </Box>
       {children}
     </Box>
